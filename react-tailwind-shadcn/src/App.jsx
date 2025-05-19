1cd0e1068ace21d8c161e1a23638119f541fdafa
subhashvadrevu <subhashv2864@gmail.com>
2025-05-19 12:44:34 +0530

    commiting react-tailwind-shadcn template

---
+++ react-tailwind-shadcn/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <>
+      <div className='text-blue-500 text-2xl'>App</div>
+      <Button>This is shadcn button component</Button>
+      <div>To add more shadcn components just copy-paste commands for that component</div>
+    </>
+  )
+}
+
+export default App
